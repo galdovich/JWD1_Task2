6ee14422241da1e1a25e2b55c137c6d3269906b8
Boroda <galdovich95@gmailcom>
2019-10-16 22:06:16 +0300

    HomeTasks

---
+++ src/PVP/Homework_4.java
@@ -0,0 +1,137 @@
+package PVP;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Homework_4<count> {
+    public static void main(String[] args) {
+        task5();
+    }
+
+    public static void task1() {
+        String input = "during the ceremony, also recalled the words that the beloved journalist said";
+        String output = "";
+        String[] array = input.split(" ");
+        for (String word : array) {
+            String first = word.substring(0, 1).toUpperCase();
+            String all = word.substring(1);
+            output += " " + first + all;
+        }
+        System.out.println(output);
+    }
+
+    public static void task2() {
+        char[] c = {'.', ',', '-', ':', ';', '?', '!'};
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < c.length; j++) {
+                if (s.charAt(i) == c[j]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void task3() {
+        System.out.print("Введите строку: ");
+        String a = new Scanner(System.in).nextLine();
+        char[] chars = a.toCharArray();
+        char[] lol = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        int sum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (lol[j] == chars[i]) {
+                    sum += Character.getNumericValue(lol[j]);
+                }
+            }
+        }
+        System.out.println("Сумма всех целых чисел в строке равна " + sum);
+    }
+
+    public static void task4() {
+        String[] text = new String[5];
+        System.out.println("Введите 5 строк: ");
+        for (int i = 0; i < 5; i++) {
+            text[i] = new Scanner(System.in).nextLine();
+        }
+
+        int symbol = 3;
+        char newChar = 'a';
+        char[] word;
+        for (int i = 0; i < text.length; i++) {
+            if (text[i].length() >= symbol) {
+                word = text[i].toCharArray();
+                word[symbol - 1] = newChar;
+                text[i] = String.copyValueOf(word);
+            }
+        }
+        System.out.println();
+
+        for (String s : text) {
+            System.out.println(s);
+        }
+    }
+
+    public static void task5() {
+        
+    }
+
+    public static void task6() {
+        String text = ("Ах как хочется жить просто жить хорошо");
+        String[] words = text.split(" ");
+        int count1 = 0;
+        int count2 = 0;
+        for (String a: words){
+            count1++;
+        }
+        for (int i = 0; i < count1; i++){
+            for (int j = 0; j < count1; j++)
+                if (words[i].equals(words[j])){
+                count2 ++;
+                }
+            System.out.println("Слово " + words [i] + " встречается " + count2 + " раз(а)");
+            count2 = 0;
+        }
+    }
+
+    public static void task7 () {
+        int count = 0;
+        Character[] vowels = new Character[]{'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'};
+        Character[] consonants = new Character[]{'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
+        String s = new Scanner(System.in).nextLine();
+        String[] words = s.split(" ,.");
+        for (int i = 0; i < words.length; i++) {
+            if (vowels.equals(words[i])) {
+                count++;
+            }
+            for (Character character : s.toCharArray()) {
+                for (Character pub : vowels) {
+                    if (character.equals(pub)) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("Гласные:" + count);
+            count = 0;
+
+            for (Character character : s.toCharArray()) {
+                for (Character lol: consonants) {
+                    if (character.equals(lol)) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("Cогласные: " + count);
+        }
+    }
+
+    public static void task8 () {
+
+        }
+    }
+
