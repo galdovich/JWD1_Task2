ef45e358d78280cd66fef2e0962efcee7c05053e
Boroda <galdovich95@gmailcom>
2019-11-23 22:23:41 +0300

    HomeTasks

---
+++ src/pvp/Semaphore/Names.java
@@ -0,0 +1,8 @@
+package pvp.Semaphore;
+
+public enum Names {
+    Liam, Emma, Noah, Olivia, Mason, Ava, Ethan, Sophia, Logan, Isabella, Lucas, Mia, Jackson,
+    Charlotte, Aiden, Amelia, Oliver, Emily, Jacob,	Madison, Elijah, Harper, Alexander,
+    Abigail, James, Avery, Benjamin, Lily, Jack, Ella, Luke, Chloe, William,
+    Evelyn, Michael, Sofia, Owen, Aria, Daniel, Ellie
+}
